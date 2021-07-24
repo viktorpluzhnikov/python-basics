bd546c275a0a6a07d0cffbcd85009c06f4bd437a
majahed228 <pluzhnikov93@inbox.ru>
2021-07-24 19:11:31 +0300

    lesson five

---
+++ lesson_5_4.py
@@ -0,0 +1,9 @@
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result = []
+for i in range(len(src) - 1):
+    n = src[i]
+    i += 1
+    m = src[i]
+    if m > n:
+        result.append(m)
+print(result)
